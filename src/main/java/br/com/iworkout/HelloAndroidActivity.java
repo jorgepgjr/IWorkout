ed6ee7b38911aae2ce9dceb0c4106bc3e13046fe
Jorge <jorgepgjr@gmail.com>
2013-06-25 23:34:29 -0300

    Ajustes de layouts e testes com a actionBar

--- src/main/java/br/com/iworkout/HelloAndroidActivity.java
+++ src/main/java/br/com/iworkout/HelloAndroidActivity.java
@@ -1,13 +1,17 @@
 package br.com.iworkout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 
 import roboguice.inject.InjectView;
 
-public class HelloAndroidActivity extends RoboSherlockActivity {
+public class HelloAndroidActivity extends RoboSherlockActivity implements ActionBar.OnNavigationListener {
     @InjectView(R.id.button) Button button;
 
     @Override
@@ -15,7 +19,28 @@ public class HelloAndroidActivity extends RoboSherlockActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button.setText("Teste do roboguice com sherlock");
+
+        //pega o conexto com o thema
+        Context context = getSupportActionBar().getThemedContext();
+        ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(context, R.array.locations, R.layout.sherlock_spinner_dropdown_item);
+        list.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
+
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        getSupportActionBar().setListNavigationCallbacks(list,this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        menu.add("Save")
+//            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+///
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        return false;
+    }
 }
 
