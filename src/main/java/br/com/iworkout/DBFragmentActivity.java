4391e6b9e8bb482b7a010917af6e1e55a79f40d5
Jorge <jorgepgjr@gmail.com>
2013-09-10 13:34:51 -0300

    Tela de treino

---
+++ src/main/java/br/com/iworkout/DBFragmentActivity.java
@@ -0,0 +1,32 @@
+package br.com.iworkout;
+
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockFragmentActivity;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+import br.com.iworkout.db.DatabaseHelper;
+
+/**
+ * Created by Jorge on 10/07/13.
+ */
+public class DBFragmentActivity extends RoboSherlockFragmentActivity{
+
+    private DatabaseHelper databaseHelper = null;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (databaseHelper != null) {
+            OpenHelperManager.releaseHelper();
+            databaseHelper = null;
+        }
+    }
+
+    protected DatabaseHelper getHelper() {
+        if (databaseHelper == null) {
+            databaseHelper =
+                    OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+}
